47a533e63d1fb8e864ca419df3f6abf4dba7121f
unknown <bertha.basico@gmail.com>
2020-05-31 21:45:53 -0500

    Chapter 10 finished

--- JavaProgramming/src/main/java/chapter9/InheritanceTester.java
+++ JavaProgramming/src/main/java/chapter9/InheritanceTester.java
@@ -1,4 +1,19 @@
 package chapter9;
 
 public class InheritanceTester {
+    public static void main(String[] args) {
+        //Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("square");
+
+        Mother mom = new Mother();
+        mom.setName("Elisa");
+
+        System.out.println(mom.getName()+ " is a " + mom.getGender());
+
+    }
 }
